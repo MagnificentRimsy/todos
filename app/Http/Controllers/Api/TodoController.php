0eef71986b5bbeb6d45fb5cf7f065f60a3c79daa
Kwame Gyimah <kwameowusugyimah@gmail.com>
2020-12-02 14:55:38 +0000

    bug fixes

--- app/Http/Controllers/Api/TodoController.php
+++ app/Http/Controllers/Api/TodoController.php
@@ -5,7 +5,9 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Facades\TodoFacade;
+use Illuminate\Http\Response;
 use App\Filters\{AuthData, Paginate, OrderByDate};
+use Illuminate\Auth\Access\AuthorizationException;
 
 class TodoController extends BaseController
 {
@@ -17,9 +19,21 @@ class TodoController extends BaseController
 	* @return mixed
 	*/
 	public function markAsComplete($id) {
-		$resource = TodoFacade::find($id);
+		$resource = $this->repo->find($id);
 
-		return response()->json(TodoFacade::markAsComplete($id));
+		if($resource[1] == Response::HTTP_OK){
+            try {
+                $this->authorize($resource[0]);
+            }catch(AuthorizationException $e) {
+                return response()->json(['message' => 'access denied'], Response::HTTP_FORBIDDEN);
+            }
+
+            return response()->json(TodoFacade::markAsComplete($id));
+            
+        }
+
+        return response()->json(['resource not found'], Response::HTTP_NOT_FOUND);
+		
 	}
     
 
