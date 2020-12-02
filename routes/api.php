5144e722afab6170f9677757031993eccbea65d1
magnificentrimsy <jonahrchirika@gmail.com>
2020-12-02 14:52:04 +0100

    routes

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,15 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::post('/signup', 'UserController@register')->name('signup');
+Route::post('/login', 'UserController@login')->name('login');
+Route::get('/user', 'UserController@getCurrentUser')->name('user');
+Route::post('/update', 'UserController@update')->name('user.update');
+Route::get('/logout', 'UserController@logout')->name('logout');
+
+
+Route::get('/task/create', 'UserController@logout')->name('task.create');
+Route::get('/tasks', 'UserController@logout')->name('task.view');
+
+
